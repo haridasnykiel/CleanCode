353ee2fe5ea0c7f7738b7b40b36f67007b59750d
Haridas <haridasnykiel@gmail.com>
2020-03-03 21:42:29 +0000

    added some examples of for bad comments

---
+++ BadComments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BadComments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee();
+
+
+            //Employee is eligible for full benefits
+            if(employee.Flag && 
+            (employee.Age > 65)) 
+            {
+                Console.WriteLine("employ is eligible");
+            }
+
+
+            if (employee.IsEligibleForFullBenefits())
+            {
+                Console.WriteLine("employ is eligible");
+            }
+        }
+    }
+
+    class Employee {
+        
+        public bool Flag {get;set;}
+        public int Age {get;set;}
+
+        public bool IsEligibleForFullBenefits() {
+            return Flag && (Age > 65);
+        }
+    }
+}
